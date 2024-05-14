b01ed854b3e1b0099961ef92072462773b1132d3
Selin Karabıçak <selinss62@gmail.com>
2024-05-14 22:18:21 +0300

    Login Page is done

--- Frontend/Web/src/Components/LoginPage/LoginPage.jsx
+++ Frontend/Web/src/Components/LoginPage/LoginPage.jsx
@@ -26,19 +26,19 @@ const LoginPage = () => {
 
   return (
     <div className="login-page">
-      <h2 className="header" >Login To Your Account</h2>
+
       <div className="login-container">
+
         <div className="left-column">
+          <h2 className="header" >Login To Your Account</h2>
           <div className="input-container">
-            <input
+            <input className='email-input'
               type="text"
               value={email}
               onChange={handleEmailChange}
               placeholder="Email"
             />
-          </div>
-          <div className="input-container">
-            <input
+            <input className='password-input'
               type="password"
               value={password}
               onChange={handlePasswordChange}
@@ -48,15 +48,18 @@ const LoginPage = () => {
           <div className="forgot-password">
             <a href="#">Forgot Password?</a>
           </div>
+          <div className='buttons'>
+            <button className="login-button" onClick={handleSubmit}>Login</button>
+            <button className="sign-up-button" onClick={handleSignUpClick}>Sign Up</button>
+          </div>
         </div>
-        <div>
-         <img className="right-column" src={Login} alt="Login" />
+
+        <div className="right-column">
+         <img  src={Login} alt="Login" />
         </div>
+
       </div>
-      <div style={{textAlign: 'left', marginTop: '-5%'}}>
-       <button className="login-button" onClick={handleSubmit}>Login</button>
-       <button className="sign-up-button" onClick={handleSignUpClick}>Sign Up</button>
-      </div>
+
     </div>
   );
 };
