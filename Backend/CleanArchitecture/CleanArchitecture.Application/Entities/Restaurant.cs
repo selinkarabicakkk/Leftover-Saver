f853346115f69dfa67053e1112a174d27ec05ada
Elif Tuzcuoğlu <el2m299@gmail.com>
2024-05-14 14:43:01 +0300

    entities edited

--- Backend/CleanArchitecture/CleanArchitecture.Application/Entities/Restaurant.cs
+++ Backend/CleanArchitecture/CleanArchitecture.Application/Entities/Restaurant.cs
@@ -9,7 +9,7 @@ namespace CleanArchitecture.Core.Entities
     public string Email { get; set; }
     public string Password { get; set; }
     public string PhoneNumber{ get; set; }
-    public int AddressID { get; set; }
+    public string AddressID { get; set; }
     public string StoreType { get; set; }
     public double Rate { get; set; }
     // Other properties
