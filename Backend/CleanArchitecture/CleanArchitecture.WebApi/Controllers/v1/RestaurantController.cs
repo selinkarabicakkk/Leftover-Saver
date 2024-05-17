61f45dcedcf7f67341b32c944f51ef7192743509
Elif Tuzcuoğlu <el2m299@gmail.com>
2024-05-17 23:21:58 +0300

    RestaurantController has been created for check restaurant listing

---
+++ Backend/CleanArchitecture/CleanArchitecture.WebApi/Controllers/v1/RestaurantController.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+[Route("api/[controller]")]
+[ApiController]
+public class RestaurantsController : ControllerBase
+{
+    ...
+}
