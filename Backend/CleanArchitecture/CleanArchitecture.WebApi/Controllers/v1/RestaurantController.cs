3a4f05b29aa39ce6215dbbc11f0811d9f7c5525c
Elif Tuzcuoğlu <el2m299@gmail.com>
2024-05-19 00:52:15 +0300

    error fix

--- Backend/CleanArchitecture/CleanArchitecture.WebApi/Controllers/v1/RestaurantController.cs
+++ Backend/CleanArchitecture/CleanArchitecture.WebApi/Controllers/v1/RestaurantController.cs
@@ -1,17 +1,30 @@
-﻿using CleanArchitecture.Core.Features.Restaurant.Command.CreateRestaurant;
-using MediatR;
+using CleanArchitecture.Core.Features.Restaurant.Command.CreateRestaurant;
+using CleanArchitecture.Core.Features.Restaurant.Query.GetAllRestaurants;
+using CleanArchitecture.Core.Wrappers;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace CleanArchitecture.WebApi.Controllers.v1
 {
-    public class RestaurantController :BaseApiController
+    [ApiVersion("1.0")]
+    public class RestaurantController : BaseApiController
     {
-        [HttpPost(Name ="CreateRestaurant")]
 
-        public async Task<int> CreateRestaurant(CreateRestaurantCommand createRestaurantCommand)
+        [HttpPost]
+        public async Task<IActionResult> Post(CreateRestaurantCommand command)
         {
-            return await Mediator.Send(createRestaurantCommand);
+            return Ok(await Mediator.Send(command));
+        }
+
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(PagedResponse<IEnumerable<GetAllRestaurantsViewModel>>))]
+        public async Task<PagedResponse<IEnumerable<GetAllRestaurantsViewModel>>> Get([FromQuery] GetAllRestaurantsParameter filter)
+        {
+            return await Mediator.Send(new GetAllRestaurantsQuery() { pageSize = filter.PageSize, pageNumber = filter.PageNumber });
         }
     }
 }
+
