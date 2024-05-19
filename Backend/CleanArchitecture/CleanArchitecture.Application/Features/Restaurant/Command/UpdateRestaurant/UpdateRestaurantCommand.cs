9e3bd4fc3e74173e05bf1e23a0d2313ba999eeec
Ebrar Tikit <94461267+EbrarTikit@users.noreply.github.com>
2024-05-19 19:42:09 +0300

    Restaurant delete,update,getById features added

---
+++ Backend/CleanArchitecture/CleanArchitecture.Application/Features/Restaurant/Command/UpdateRestaurant/UpdateRestaurantCommand.cs
@@ -0,0 +1,53 @@
+﻿using CleanArchitecture.Core.Exceptions;
+using CleanArchitecture.Core.Features.Restaurant.Command.DeleteRestaurant;
+using CleanArchitecture.Core.Interfaces.Repositories;
+using CleanArchitecture.Core.Wrappers;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using CleanArchitecture.Core.Entities;
+using CleanArchitecture.Core.DTOs.Restaurant;
+
+namespace CleanArchitecture.Core.Features.Restaurant.Command.UpdateRestaurant
+{
+    public class UpdateRestaurantCommand : IRequest<Response<int>>
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } 
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string PhoneNumber { get; set; }
+        public string AddressID { get; set; }
+        public string StoreType { get; set; }
+        public double Rate { get; set; }
+        public class UpdateRestaurantCommandHandler : IRequestHandler<UpdateRestaurantCommand, Response<int>>
+        {
+            private readonly IRestaurantRepositoryAsync _restaurantRepository;
+            public UpdateRestaurantCommandHandler(IRestaurantRepositoryAsync restaurantRepository)
+            {
+                _restaurantRepository = restaurantRepository;
+            }
+            public async Task<Response<int>> Handle(UpdateRestaurantCommand command, CancellationToken cancellationToken)
+            {
+                var restaurant = await _restaurantRepository.GetByIdAsync(command.Id);
+
+                if (restaurant == null) throw new EntityNotFoundException("restaurant", command.Id);
+
+                restaurant.Name = command.Name;
+                restaurant.Rate = command.Rate;
+                restaurant.Email = command.Email;
+                restaurant.Password = command.Password;
+                restaurant.PhoneNumber = command.PhoneNumber;
+                restaurant.AddressID = command.AddressID;
+                restaurant.StoreType = command.StoreType;
+
+                await _restaurantRepository.UpdateAsync(restaurant);
+
+                return new Response<int>(restaurant.Id);
+            }
+        }
+    }
+}
---
+++ Backend/CleanArchitecture/CleanArchitecture.Application/Features/Restaurant/Query/GetRestaurantById/GetRestaurantByIdQuery.cs
@@ -0,0 +1,32 @@
+﻿using CleanArchitecture.Core.Exceptions;
+using CleanArchitecture.Core.Wrappers;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using CleanArchitecture.Core.Interfaces.Repositories;
+using CleanArchitecture.Core.Entities;
+
+namespace CleanArchitecture.Core.Features.Restaurant.Query.GetRestaurantById
+{
+    public class GetRestaurantByIdQuery : IRequest<Response<Entities.Restaurant>>
+    {
+        public int Id { get; set; }
+        public class GetRestaurantByIdQueryHandler : IRequestHandler<GetRestaurantByIdQuery, Response<Entities.Restaurant>>
+        {
+            private readonly IRestaurantRepositoryAsync _restaurantRepository;
+            public GetRestaurantByIdQueryHandler(IRestaurantRepositoryAsync restaurantRepository)
+            {
+                _restaurantRepository = restaurantRepository;
+            }
+            public async Task<Response<Entities.Restaurant>> Handle(GetRestaurantByIdQuery query, CancellationToken cancellationToken)
+            {
+                var restaurant = await _restaurantRepository.GetByIdAsync(query.Id);
+                if (restaurant == null) throw new ApiException($"Restaurant Not Found.");
+                return new Response<Entities.Restaurant>(restaurant);
+            }
+        }
+    }
+}
